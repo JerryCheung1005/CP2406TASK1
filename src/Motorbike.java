1c740a0151ca6d46b6b8aa4a9326c29217455933
JerryCheung1005 <JerryCheung1005@users.noreply.github.com>
2021-04-10 18:28:42 +0800

    Make changes

--- src/Motorbike.java
+++ src/Motorbike.java
@@ -1,2 +1,8 @@
-public class Motorbike {
+public class Motorbike extends Car {
+
+    public Motorbike(String id) {
+        this.id = ("bike_" + id);
+        length = super.getLength() * 0.5f;
+    }
+
 }
