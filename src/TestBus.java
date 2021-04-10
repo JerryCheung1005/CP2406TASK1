b4e03cda01169f3b28d9bce224a619bbdb567aca
JerryCheung1005 <JerryCheung1005@users.noreply.github.com>
2021-04-10 18:39:22 +0800

    Make changes

--- src/TestBus.java
+++ src/TestBus.java
@@ -1,2 +1,24 @@
-public class TestBus {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestBus {
+    Road road = new Road("0", 1, 5, new int[]{0, 0});
+    Bus bus = new Bus("0");
+
+    @Test
+    void getLength() {
+        assertEquals(3, bus.getLength());
+    }
+
+    @Test
+    void getId() {
+        assertEquals("bus_0", bus.getId());
+    }
+
+    @Test
+    void testInheritance() {
+        assertEquals(0, bus.getSpeed());
+        assertEquals(1, bus.getPosition());
+    }
 }
